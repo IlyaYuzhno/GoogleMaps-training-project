972b266238a70e52f0125e9ead144f4bbd08e4d0
Ilya Doroshkevich <kardor@mail.ru>
2021-04-02 15:52:02 +0300

    HW2

---
+++ HW1/App/Entity/CoordinatesToStore.swift
@@ -0,0 +1,16 @@
+//
+//  CoordinatesToStore.swift
+//  HW1
+//
+//  Created by Ilya Doroshkevitch on 02.04.2021.
+//
+
+import Foundation
+import RealmSwift
+
+
+class CoordinatesToStore: Object {
+
+    @objc dynamic var toStore = ""
+
+}
